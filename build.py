2e959e25a0787bdc916783664b3e625e9486e1a0
David Given <dg@cowlark.com>
2024-12-21 16:22:00 +0000

    Fetch Maven and HTTP artifacts during the build.

--- build.py
+++ build.py
@@ -7,7 +7,14 @@ from build.ab import (
     filenameof,
     filenamesof,
 )
-from build.java import javalibrary, externaljar, javaprogram, javalink
+from build.java import (
+    javalibrary,
+    externaljar,
+    javaprogram,
+    javalink,
+    mavenjar,
+    httpjar,
+)
 from build.utils import itemsof
 from build.protobuf import protojava, proto
 from build.zip import zip
@@ -15,64 +22,78 @@ from os.path import *
 from glob import glob
 import re
 
-EXTERNAL_JARS = [
-    "TimingFramework",
-    "asm",
-    "antlr3",
-    "antlr3-runtime",
-    "bcpkix",
-    "bcutil",
-    "bnd",
-    "bsaf",
-    "commons-collections4",
-    "commons-compress",
-    "commons-dbcp2",
-    "commons-io",
-    "commons-lang3",
-    "commons-text",
-    "gson",
-    "h2",
-    "isorelax",
-    "jdom1",
-    "jgrapht-core",
-    "jgrapht-io",
-    "jh",
-    "jna",
-    "jna-platform",
-    "jsch",
-    "json-simple",
-    "jung-algorithms",
-    "jung-visualization",
-    "junit4",
-    "jython",
-    "log4j-api",
-    "log4j-core",
-    "msv-core",
-    "org.apache.felix.framework",
-    "org.apache.felix.utils",
-    "osgi.core",
-    "protobuf",
-    "smali",
-    "xpp3",
-    "baksmali",
-]
+EXTERNAL_JARS = {
+    "TimingFramework": "net.java.dev.timingframework:timingframework:1.0",
+    "antlr3-runtime": "org.antlr:antlr-runtime:3.5.2",
+    "asm": "org.ow2.asm:asm-debug-all:5.0.3",
+    "baksmali": "org.smali:baksmali:2.5.2",
+    "bcpkix": "org.bouncycastle:bcpkix-jdk15on:1.69",
+    "bcprov": "org.bouncycastle:bcprov-jdk15on:1.69",
+    "bcutil": "org.bouncycastle:bcutil-jdk15on:1.69",
+    "bnd": "biz.aQute.bnd:biz.aQute.bnd.util:7.0.0",
+    "bndlib": "biz.aQute.bnd:biz.aQute.bndlib:7.0.0",
+    "commons-collections4": "org.apache.commons:commons-collections4:4.4",
+    "commons-compress": "org.apache.commons:commons-compress:1.27.1",
+    "commons-dbcp2": "org.apache.commons:commons-dbcp2:2.9.0",
+    "commons-io": "commons-io:commons-io:2.11.0",
+    "commons-lang3": "org.apache.commons:commons-lang3:3.17.0",
+    "commons-text": "org.apache.commons:commons-text:1.10.0",
+    "dex-reader": "de.femtopedia.dex2jar:dex-reader:2.4.24",
+    "dex-reader-api": "de.femtopedia.dex2jar:dex-reader-api:2.4.24",
+    "dex-translator": "de.femtopedia.dex2jar:dex-translator:2.4.24",
+    "dexlib2": "org.smali:dexlib2:2.5.2",
+    "flatlaf": "com.formdev:flatlaf:3.2.1",
+    "gson": "com.google.code.gson:gson:2.9.0",
+    "guava": "com.google.guava:guava:32.1.3-jre",
+    "h2": "com.h2database:h2:2.2.220",
+    "isorelax": "msv:isorelax:20050913",
+    "jdom1": "org.jdom:jdom-legacy:1.1.3",
+    "jgrapht-core": "org.jgrapht:jgrapht-core:1.5.1",
+    "jgrapht-io": "org.jgrapht:jgrapht-io:1.5.1",
+    "jh": "javax.help:javahelp:2.0.05",
+    "jna": "net.java.dev.jna:jna:5.14.0",
+    "jna-platform": "net.java.dev.jna:jna-platform:5.14.0",
+    "jsch": "com.jcraft:jsch:0.1.55",
+    "json-simple": "com.googlecode.json-simple:json-simple:1.1.1",
+    "jung-algorithms": "net.sf.jung:jung-algorithms:2.1.1",
+    "jung-api": "net.sf.jung:jung-api:2.1.1",
+    "jung-graph-impl": "net.sf.jung:jung-graph-impl:2.1.1",
+    "jung-visualization": "net.sf.jung:jung-visualization:2.1.1",
+    "jungrapht-io": "org.jgrapht:jgrapht-io:1.5.1",
+    "jungrapht-layout": "com.github.tomnelson:jungrapht-layout:1.4",
+    "jungrapht-visualization": "com.github.tomnelson:jungrapht-visualization:1.4",
+    "junit4": "junit:junit:4.12",
+    "log4j-api": "org.apache.logging.log4j:log4j-api:2.17.1",
+    "log4j-core": "org.apache.logging.log4j:log4j-core:2.17.1",
+    "msv": "msv:msv:20050913",
+    "olcut-protobuf": "com.oracle.labs.olcut:olcut-config-protobuf:5.2.0",
+    "org.apache.felix.framework": "org.apache.felix:org.apache.felix.framework:7.0.5",
+    "org.apache.felix.utils": "org.apache.felix:org.apache.felix.utils:7.0.5",
+    "osgi.core": "org.osgi:osgi.core:8.0.0",
+    "osgi.promise": "org.osgi:org.osgi.util.promise:1.3.0",
+    "phidias": "com.github.rotty3000:phidias:0.3.7",
+    "protobuf": "com.google.protobuf:protobuf-java:3.19.6",
+    "sevenzipjbinding": "net.sf.sevenzipjbinding:sevenzipjbinding:16.02-2.01",
+    "sevenzipjbinding": "net.sf.sevenzipjbinding:sevenzipjbinding:16.02-2.01",
+    "slf4j-api": "org.slf4j:slf4j-api:1.7.25",
+    "smali": "org.smali:util:2.5.2",
+    "xpp3": "org.ogce:xpp3:1.1.6",
+    "xz": "org.tukaani:xz:1.9",
+#"antlr3",
+#"bsaf",
+#"jython",
+}
 
-EXTRA_JARS = [
-    "jungrapht-layout-1.4",
-    "jungrapht-visualization-1.4",
-    "phidias-0.3.7",
-    "dex-reader-2.4.18",
-    "dex-reader-api-2.4.18",
-    "dex-translator-2.4.18",
-    "axmlprinter2-2016-07-27",
-    "sevenzipjbinding-16.02-2.01",
-    "java-sarif-2.0",
-]
+for name, artifact in EXTERNAL_JARS.items():
+    mavenjar(name=name, artifact=artifact)
 
-for j in EXTERNAL_JARS:
-    externaljar(name=j, paths=[f"/usr/share/java/{j}.jar"])
-for j in EXTRA_JARS:
-    externaljar(name=j, paths=[f"{j}.jar"])
+httpjar(
+    name="java-sarif",
+    url="https://github.com/NationalSecurityAgency/ghidra-data/raw/refs/heads/main/lib/java-sarif-2.1-modified.jar"
+)
+httpjar(
+    name="axmlprinter2",
+    url="https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/android4me/AXMLPrinter2.jar")
 
 allmodules = {}
 
@@ -119,9 +140,20 @@ ghidramodule(
         ".+gson",
         ".+bcpkix",
         ".+bcutil",
+        ".+bcprov",
     ],
 )
-ghidramodule("Framework/Graph", deps=[".+jung-visualization", ".+jgrapht-core"])
+ghidramodule(
+    "Framework/Graph",
+    deps=[
+        ".+guava",
+        ".+jung-api",
+        ".+jung-visualization",
+        ".+jung-algorithms",
+        ".+jung-graph-impl",
+        ".+jgrapht-core",
+    ],
+)
 ghidramodule("Framework/Gui")
 ghidramodule("Framework/Help", deps=[".+jh"])
 ghidramodule("Framework/Project")
@@ -135,7 +167,7 @@ ghidramodule(
         ".+sleighparser_antlr",
         ".+sleighecho_antlr",
         ".+sleighcompiler_antlr",
-        ".+msv-core",
+        ".+msv",
         ".+antlr3-runtime",
         ".+isorelax",
     ],
@@ -149,7 +181,9 @@ ghidramodule(
         ".+osgi.core",
         ".+bnd",
         ".+org.apache.felix.framework",
-        ".+phidias-0.3.7",
+        ".+phidias",
+        ".+bnd",
+        ".+bndlib",
     ],
 )
 ghidramodule("Features/BSim", deps=[".+commons-dbcp2", ".+json-simple", ".+h2"])
@@ -164,13 +198,15 @@ ghidramodule(
     "Features/FileFormats",
     deps=[
         ".+xpp3",
-        ".+dex-reader-2.4.18",
-        ".+dex-reader-api-2.4.18",
-        ".+dex-translator-2.4.18",
+        ".+dex-reader",
+        ".+dex-reader-api",
+        ".+dex-translator",
+        ".+dexlib2",
         ".+baksmali",
         ".+asm",
-        ".+axmlprinter2-2016-07-27",
-        ".+sevenzipjbinding-16.02-2.01",
+        ".+axmlprinter2",
+        ".+sevenzipjbinding",
+        ".+xz",
     ],
 )
 ghidramodule("Features/FunctionGraph")
@@ -185,8 +221,8 @@ ghidramodule(
     deps=[
         ".+jgrapht-core",
         ".+jgrapht-io",
-        ".+jungrapht-visualization-1.4",
-        ".+jungrapht-layout-1.4",
+        ".+jungrapht-visualization",
+        ".+jungrapht-layout",
     ],
 )
 ghidramodule("Features/MicrosoftCodeAnalyzer")
@@ -195,9 +231,9 @@ ghidramodule("Features/MicrosoftDmang")
 ghidramodule("Features/PDB")
 ghidramodule("Features/ProgramDiff")
 ghidramodule("Features/ProgramGraph")
-# ghidramodule("Features/Python", deps=[".+jython"])
+#ghidramodule("Features/Python", deps = [".+jython"])
 ghidramodule("Features/Recognizers")
-ghidramodule("Features/Sarif", deps=[".+java-sarif-2.0"])
+ghidramodule("Features/Sarif", deps=[".+java-sarif"])
 ghidramodule("Features/SourceCodeLookup")
 ghidramodule("Features/SystemEmulation")
 ghidramodule("Features/VersionTracking")
@@ -331,7 +367,7 @@ export(
         "dist/support/LaunchSupport.jar": "+launchsupport",
         "dist/decompile": "Ghidra/Features/Decompiler/src/decompile+decompile",
         "dist/Ghidra/application.properties": "Ghidra/application.properties",
-        # "sleigh": "Ghidra/Features/Decompiler/src/decompile+sleigh",
+#"sleigh" : "Ghidra/Features/Decompiler/src/decompile+sleigh",
     }
     | {
         f"dist/Ghidra/{root}/lib/{basename(root)}.jar": t
---
+++ build/_manifest.py
@@ -0,0 +1,3 @@
+import sys
+
+    for v in sys.argv[1 : ] :first = True while v:print(("" if first else " ") + v[ : 70]) v = v[70 : ] first = False
